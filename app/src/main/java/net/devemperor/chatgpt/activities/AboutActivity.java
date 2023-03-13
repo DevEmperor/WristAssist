efe533c1aac7697e37ef69085b81b47d32d10208
DevEmperor <56255079+devemperor@users.noreply.github.com>
2023-03-13 12:35:32 +0100

    Added AboutActivity (forgot to add files to git 🙈)

---
+++ app/src/main/java/net/devemperor/chatgpt/activities/AboutActivity.java
@@ -0,0 +1,16 @@
+package net.devemperor.chatgpt.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.devemperor.chatgpt.R;
+
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
