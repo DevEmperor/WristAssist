25ae7df690062b4927e301d26cf7d796c8971a31
DevEmperor <56255079+devemperor@users.noreply.github.com>
2023-03-16 20:46:49 +0100

    Larger buttons, AboutActivity automatically gets version string

--- app/src/main/java/net/devemperor/chatgpt/activities/AboutActivity.java
+++ app/src/main/java/net/devemperor/chatgpt/activities/AboutActivity.java
@@ -2,7 +2,9 @@ package net.devemperor.chatgpt.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import net.devemperor.chatgpt.BuildConfig;
 import net.devemperor.chatgpt.R;
 
 
@@ -12,5 +14,8 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView aboutText = findViewById(R.id.version_tv);
+        aboutText.setText(getString(R.string.chatgpt_about, BuildConfig.VERSION_NAME));
     }
 }
