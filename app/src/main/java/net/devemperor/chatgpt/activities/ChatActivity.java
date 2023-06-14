22fa7e324505e1dd14e36fc45b15b926397dfaab
DevEmperor <56255079+devemperor@users.noreply.github.com>
2023-06-14 14:25:38 +0200

    Gradle upgrade to 8.0.2

--- app/src/main/java/net/devemperor/chatgpt/activities/ChatActivity.java
+++ app/src/main/java/net/devemperor/chatgpt/activities/ChatActivity.java
@@ -229,7 +229,7 @@ public class ChatActivity extends Activity {
         }
 
         ChatCompletionRequest ccr = ChatCompletionRequest.builder()
-                .model("gpt-3.5-turbo-0301")
+                .model("gpt-3.5-turbo")
                 .messages(chatAdapter.getChatMessages())
                 .build();
 
