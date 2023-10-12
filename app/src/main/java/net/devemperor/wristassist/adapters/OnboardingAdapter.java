2f759200f3b98f8069787f68da3a7f5dbdc33f57
DevEmperor <56255079+devemperor@users.noreply.github.com>
2023-10-12 19:07:53 +0200

    splash icon & font adaption for onboarding

--- app/src/main/java/net/devemperor/wristassist/adapters/OnboardingAdapter.java
+++ app/src/main/java/net/devemperor/wristassist/adapters/OnboardingAdapter.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import net.devemperor.wristassist.R;
 import net.devemperor.wristassist.activities.MainActivity;
+import net.devemperor.wristassist.util.Util;
 
 public class OnboardingAdapter extends RecyclerView.Adapter<OnboardingAdapter.ViewHolder> {
 
@@ -33,7 +35,13 @@ public class OnboardingAdapter extends RecyclerView.Adapter<OnboardingAdapter.Vi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        if (position == layoutIds.length - 1) {
+        if (position == 0) {
+            TextView welcomeTv = holder.itemView.findViewById(R.id.welcome_tv);
+            welcomeTv.setTextSize(20 * Util.getFontMultiplier(welcomeTv.getContext()));
+        } else if (position == 1) {
+            TextView infoTv = holder.itemView.findViewById(R.id.info_tv);
+            infoTv.setTextSize(16 * Util.getFontMultiplier(infoTv.getContext()));
+        } else if (position == 2) {
             ImageView qrCodeIv = holder.itemView.findViewById(R.id.qrcode_iv);
             qrCodeIv.setOnClickListener(v -> {
                 activity.getSharedPreferences("net.devemperor.wristassist", Activity.MODE_PRIVATE)
