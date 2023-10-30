a7936ce7cc4e49698d85be3fdb5c050d384e5f55
DevEmperor <56255079+devemperor@users.noreply.github.com>
2023-10-30 17:27:26 +0100

    Bugfix: pawan.krd works again

--- app/src/main/java/net/devemperor/wristassist/activities/PreferencesFragment.java
+++ app/src/main/java/net/devemperor/wristassist/activities/PreferencesFragment.java
@@ -6,6 +6,7 @@ import android.text.InputType;
 import android.text.TextUtils;
 
 import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -34,6 +35,32 @@ public class PreferencesFragment extends PreferenceFragmentCompat {
                 editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
                 editText.setSingleLine(true);
             });
+        }
+
+        ListPreference model = findPreference("net.devemperor.wristassist.model");
+        ListPreference apiHost = findPreference("net.devemperor.wristassist.api_host");
+        if (model != null && apiHost != null) {
+            apiHost.setOnPreferenceChangeListener((preference, newValue) -> {
+                String newModel = model.getValue();
+                if (newValue.equals("https://api.openai.com/")) {
+                    model.setEntries(R.array.models_openai);
+                    model.setEntryValues(R.array.models_openai_values);
+                    newModel = "gpt-3.5-turbo";
+                }
+                if (newValue.equals("https://api.pawan.krd/")) {
+                    model.setEntries(R.array.models_pawan);
+                    model.setEntryValues(R.array.models_pawan_values);
+                    newModel = "pai-001-light-beta";
+                }
+                getPreferenceManager().getSharedPreferences().edit().putString("net.devemperor.wristassist.model", newModel).apply();
+                model.setValueIndex(0);
+                return true;
+            });
+            apiHost.callChangeListener(apiHost.getValue());
+
+            apiHost.setSummaryProvider(preference -> apiHost.getEntry());
+
+            model.setSummaryProvider(preference -> model.getEntry());
         }
 
         new TextToSpeech(getContext(), status -> {
