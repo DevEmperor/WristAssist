f0f41059e13b58a7c0acf2091c6f3295ca223f84
DevEmperor <56255079+devemperor@users.noreply.github.com>
2024-02-02 15:50:30 +0100

    design changes: scrollable EditChatActivity

--- app/src/main/java/net/devemperor/wristassist/activities/EditChatActivity.java
+++ app/src/main/java/net/devemperor/wristassist/activities/EditChatActivity.java
@@ -10,12 +10,10 @@ import android.widget.Toast;
 
 import net.devemperor.wristassist.R;
 import net.devemperor.wristassist.database.ChatHistoryDatabaseHelper;
-import net.devemperor.wristassist.util.Util;
 
 import org.json.JSONException;
 
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -29,7 +27,6 @@ public class EditChatActivity extends Activity {
 
     ChatHistoryDatabaseHelper chatHistoryDatabaseHelper;
     long id;
-    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +43,12 @@ public class EditChatActivity extends Activity {
         id = getIntent().getLongExtra("net.devemperor.wristassist.chatId", -1);
 
         titleTv.setText(chatHistoryDatabaseHelper.getTitle(id));
-        titleTv.setTextSize(16 * Util.getFontMultiplier(this));
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd\nHH:mm:ss", Locale.getDefault());
-        modifiedTv.setText(formatter.format(chatHistoryDatabaseHelper.getModified(id)));
-        modifiedTv.setTextSize(14 * Util.getFontMultiplier(this));
+        modifiedTv.setText(getString(R.string.wristassist_formatted_date, formatter.format(chatHistoryDatabaseHelper.getModified(id))));
 
         try {
-            chatCostTv.setText(getString(R.string.wristassist_chat_cost, df.format(chatHistoryDatabaseHelper.getChatCost(this, id) / 1000.0)));
-            chatCostTv.setTextSize(14 * Util.getFontMultiplier(this));
+            chatCostTv.setText(getString(R.string.wristassist_chat_cost, String.format(Locale.getDefault(), "%,d", chatHistoryDatabaseHelper.getChatCost(this, id))));
         } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
         }
